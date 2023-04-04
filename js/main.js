d737696458414aa55ea1995f64bd608bac5eedcf
sheriffcodes <callmezzak@gmail.com>
2023-04-04 19:44:06 +0100

    space picture hosted

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=qWCRPmj5gDod3u6MJFLykXRQVMAmMrGfIU2iV1my&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data.media_type === 'image'){
+
+          document.querySelector('img').src = data.hdurl
+          document.querySelector('.explanation').style.padding = '1rem'
+          document.querySelector('iframe').src = ''
+        }else if (data.media_type === 'video'){
+          document.querySelector('img').src = ''
+          document.querySelector('iframe').src = data.url
+        }
+        document.querySelector('.explanation').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
